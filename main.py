389add1cc806f93bb6ad304393322b93b85531ed
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2022-07-27 13:18:46 +0300

    'port'

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def start():
     return 'hello world'  # craping_telegramchats.main()
 
 if __name__ == "__main__":
-    app.run(debug=True)  #(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
 
 
 
