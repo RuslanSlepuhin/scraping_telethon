81b3278accdeb9e4e16ee4b839c64b10e9426aec
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2022-08-09 14:12:42 +0300

    09.08

--- links.py
+++ links.py
@@ -1,17 +1,17 @@
 list_links = [
-    # 'https://t.me/relocaty_jobs',  # +
-    # 'https://t.me/qa_jobs',  # +
-    # 'https://t.me/scala_jobs_feed',
-    # 'https://t.me/joinchat/AAAAAEzV8yADoj9hGdvThQ', # подписаться
-    # 'https://t.me/qajoboffer',
-    # 'https://t.me/javascript_jobs_feed',
-    # 'https://t.me/joinchat/AAAAAE41V66UBtfXtuSrpg',  # подписаться
-    # 'https://t.me/itjobsinam',  # +
-    # 'https://t.me/remotejun',
-    # 'https://t.me/hiddengurus',
-    # 'https://t.me/it_vac',
-    # 'https://t.me/belit_jobs',  # +
-    # 'https://t.me/hr_itwork',
+    'https://t.me/relocaty_jobs',  # +
+    'https://t.me/qa_jobs',  # +
+    'https://t.me/scala_jobs_feed',
+    'https://t.me/joinchat/AAAAAEzV8yADoj9hGdvThQ', # подписаться
+    'https://t.me/qajoboffer',
+    'https://t.me/javascript_jobs_feed',
+    'https://t.me/joinchat/AAAAAE41V66UBtfXtuSrpg',  # подписаться
+    'https://t.me/itjobsinam',  # +
+    'https://t.me/remotejun',
+    'https://t.me/hiddengurus',
+    'https://t.me/it_vac',
+    'https://t.me/belit_jobs',  # +
+    'https://t.me/hr_itwork',
     'https://t.me/relocateme',
     'https://t.me/nskoffice',
     'https://t.me/seohr',
