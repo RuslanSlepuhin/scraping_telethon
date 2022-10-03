1651437aa3c5949bd1e5e7fb961ab20ea4443d85
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2022-10-03 15:31:14 +0300

    web

---
+++ pattern_Ruslan.py
@@ -0,0 +1,51 @@
+pattern = {
+            'backend': {'drupal', 'бэкэнд', 'бэкенд', 'бэк-энд', 'бэк-енд', 'бекенд',
+                           'python', 'linux', 'django', 'docker', 'linux', 'websocket',
+                           'pandas', 'flask', r'\Wrust', 'goland', 'go developer', 'symfony', 'ruby', 'elixir', 'rest api',
+                        'restapi', 'ruby on rails', 'ror', 'c#', 'с#', '.net', 'rails', 'numpy', 'redis',
+                        'веб-разработчик', 'веб разработчик', 'веб разработка', 'node.js', 'nodejs', 'aws'},
+
+            'frontend': {'front end', 'фронтэнд', 'фронтенд', 'фронт-энд', 'фронт-енд',
+                            'фронт энд', 'фронт енд', 'javascript', 'html', 'firebase', 'vue.js', 'vuejs',
+                            'ether.js', 'etherjs', 'web3.js', 'web3js', 'css', 'jquery', 'ajax'},
+
+            'qa': {'qa-', 'qa/', 'qa fullstack', 'manual', 'qaengineer', 'qa engineer', 'тестировщик', 'test ', 'Test Automation Engineer',
+                   'automatic testing', 'автоматизация процессов тестирования', 'тестировщика', 'тестирования', 'автотестов',
+                   'тестированию', 'автоматизация тестирования', 'автотестировании', 'ручном тестировании', 'тестировании', 'auto',
+                   'автоматизатора', 'автоматизатор', 'автоматизации тестирования', 'test automation', 'инженер ручного тестирования', 'тестирования', 'AutomationQA'},
+
+            'fullstack': {'фуллстэк', 'фуллстек', 'фулстэк', 'фулстек'},
+
+            'analyst': {},
+
+            'sales_manager': {},
+
+            'designer': {'uikit', 'гейм-дизайнер', 'геймдизайнер'},
+
+            'mobile': {'андроид',},
+
+            'pm': {'projectmanager', 'project/manager', 'pm ', 'проджект менеджер', 'проджект-менеджер', 'менеджерпроекта'},
+
+            'product': {'product-manager', 'productmanager', 'продакт менеджер', 'продукт менеджер',
+                        'подакт-менеджер', 'продукт-менеджер', 'продактменеджер', 'продуктменеджер', 'business development manager', 'business development'},
+
+            'game': {' unity', 'match-3', 'match3', 'pipeline', 'unreal engine'},
+
+            'ba': {'*BA ', 'бизнес аналитика', 'бизнесаналитик', 'analyst', ' domo'},
+
+            'devops': {'dev ops', 'девопс', 'дев опс', 'database reliability engineer'},
+
+            'marketing': {'sas marketing automation'},
+
+            'hr': {},
+
+            'ad': {'резюме', 'cv ', 'ищу работу', 'ищуработу', 'opentowork', 'фильм на вечер', 'рекомендую', 'хотим рассказать о новых каналах',
+                    'skillbox', 'зарабатывать на крипте', 'секретар', 'делопроизводител',
+                    'онлайн курс', 'образовательная платформа', 'со скидкой',
+                   'бесплатном марафоне', 'это помогает нам стать лучше для вас', 'получайте больше откликов', '3dartist', '3d artist',
+                   'бесплатном интенсиве', 'бесплатный интенсив', 'админвещает', 'в онлайн-интенсиве', 'обо мне', 'ish joyi kerak',
+                   'geekjob.ru', 'мы не ищем сотрудников', 'поиске работы', 'sales manager', 'salesmanager',
+                   'sales_manager', '‼️Как работает этот канал:', 'outstaff'},
+
+            'vacancy': {'Your Responsibilities', }
+}
